7bff71dff7bb8396639ba4d39d9504a8394a5bee
lamfire <52089489@qq.com>
2018-05-09 17:05:44 +0800

    add hard code config sample

---
+++ src/test/java/com/lamfire/jmongo/test/HardCodeConfigTest.java
@@ -0,0 +1,32 @@
+package com.lamfire.jmongo.test;
+
+import com.lamfire.jmongo.JMongo;
+import com.lamfire.jmongo.JMongoZoneOptions;
+import com.lamfire.jmongo.JMongoZoneRegistry;
+import com.lamfire.jmongo.dao.DAO;
+import com.lamfire.jmongo.test.entity.User;
+
+import java.net.UnknownHostException;
+
+/**
+ * Created by linfan on 2018/5/9.
+ */
+public class HardCodeConfigTest {
+    public static void main(String[] args) throws UnknownHostException {
+        String zone = "hardcodeDB";
+        JMongoZoneOptions options = new JMongoZoneOptions(zone);
+        options.addHost("10.18.98.228:34300");
+        options.setConnectionsPerHost(4);
+        JMongo.registerZoneOptions(options);
+
+        DAO<User,String> userDAO  = JMongo.getDAO(zone,"HardCodeDB","User", User.class);
+
+        User user = new User();
+        user.setId("1000001");
+        user.setNickname("lamfire");
+        user.setAge(18);
+        userDAO.save(user);
+
+        System.out.println(userDAO.count());
+    }
+}
