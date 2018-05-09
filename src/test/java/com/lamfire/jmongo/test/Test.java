95b0fc8362418fef014d46d9e6b69d6f29c96e74
lamfire <52089489@qq.com>
2018-05-09 18:14:10 +0800

    add readme.md

--- src/test/java/com/lamfire/jmongo/test/Test.java
+++ src/test/java/com/lamfire/jmongo/test/Test.java
@@ -4,9 +4,6 @@ import com.lamfire.jmongo.JMongo;
 import com.lamfire.jmongo.dao.DAO;
 import com.lamfire.jmongo.test.entity.User;
 
-/**
- * Created by linfan on 2017/4/21.
- */
 public class Test {
     public static void main(String[] args) {
         DAO<User,String> dao = JMongo.getDAO("db0","test","USER0",User.class);
